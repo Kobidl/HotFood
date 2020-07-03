c802aa828c38cc552914ddb93fc696cbd6dbd514
Kobi <kobidalal1@gmail.com>
2020-07-03 16:47:00 +0300

    added login controller

--- src/com/hotfood/views/MainView.java
+++ src/com/hotfood/views/MainView.java
@@ -7,22 +7,26 @@ import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
+import com.hotfood.controllers.LoginController;
+
 public class MainView extends JFrame {
 
 	public MainView() {
 		this.setTitle("HOTFOOD");
 		this.setBounds(100, 100, 546, 498);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JLayeredPane layeredPane = new JLayeredPane();
 		this.getContentPane().add(layeredPane, BorderLayout.CENTER);
-		
+
 		LoginView loginPane = new LoginView();
+		LoginController loginController = new LoginController(loginPane);
 		loginPane.setBounds(0, 0, 546, 498);
-		
+
+		layeredPane.removeAll();
 		layeredPane.add(loginPane);
 		layeredPane.repaint();
 		layeredPane.revalidate();
-		
+
 	}
 }
