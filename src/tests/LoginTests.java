2d3b4167aa5aef7d4e4f80f920dbe7e724f6de2c
Kobi <kobidalal1@gmail.com>
2020-07-10 12:38:17 +0300

    login tests

--- src/tests/LoginTests.java
+++ src/tests/LoginTests.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.UUID;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.hotfood.enums.RegisterStatus;
@@ -13,39 +15,52 @@ import com.hotfood.models.LoginModel;
 import com.hotfood.models.User;
 
 class LoginTests {
-
+	private static  LoginModel loginModel;
+	private String email;
+	private String name;
+	private String password;
+	
+	
+	@BeforeAll
+	public static void init() {
+		loginModel = new LoginModel();
+	}
+	
+	@BeforeEach
+	public void beforeTest(){
+		name = UUID.randomUUID().toString().replace('-', '_');
+		email = name+"@test.com";
+		password = "123456";
+	}
+	
 	@Test
 	void validLogin() {
-		LoginModel loginModel = new LoginModel();
 		User user = loginModel.login("1", "1");
 		assertNotNull(user);
 		user.getName();
 	}
 	
 	@Test
-	void badLogin() {
-		LoginModel loginModel = new LoginModel();
+	void emptyLogin() {
 		User user = loginModel.login("", "");
 		assertNull(user);
 	}
 	
 	@Test
+	void emptyRegistration() {
+		RegisterStatus status = loginModel.register("", "",0, "");
+		assertTrue(status != RegisterStatus.Success);
+	}
+	
+	@Test
 	void registerValidCustomer() {
-		LoginModel loginModel = new LoginModel();
-		String name = UUID.randomUUID().toString().replace('-', '_');
-		String email = name+"@test.com";
-		String password = "123";
-		RegisterStatus status = loginModel.register(email, "123", 1, name);
+		RegisterStatus status = loginModel.register(email, password, 1, name);
 		assertEquals(RegisterStatus.Success, status,"User not created altought suppose to be created");
 	}
 	
 	@Test
 	void registerCustomerAndLogin() {
-		LoginModel loginModel = new LoginModel();
-		String name = UUID.randomUUID().toString().replace('-', '_');
-		String email = name+"@test.com";
-		String password = "123";
-		RegisterStatus status = loginModel.register(email, "123", 1, name);
+		RegisterStatus status = loginModel.register(email, password, 1, name);
 		assertEquals(RegisterStatus.Success, status,"User not created altought suppose to be created");
 		
 		User user = loginModel.login(email, password);
@@ -57,6 +72,50 @@ class LoginTests {
 
 	}
 	
+	@Test
+	void registerValidResturant() {
+		RegisterStatus status = loginModel.register(email, password, 0, name);
+		assertEquals(RegisterStatus.Success, status,"User not created altought suppose to be created");
+	}
 	
+	@Test
+	void registerResturantAndLogin() {
+		RegisterStatus status = loginModel.register(email, password, 0, name);
+		assertEquals(RegisterStatus.Success, status,"User not created altought suppose to be created");
+		
+		User user = loginModel.login(email, password);
+		assertNotNull(user,"Failed to login to user after register");
+		assertEquals(name,user.getName());
+		assertEquals(email,user.getEmail());
+		assertEquals(password,user.getPassword());
+		assertEquals(UserType.Customer,user.getType());
+	}
 	
+	@Test
+	void registerBadEmail() {
+		RegisterStatus status = loginModel.register("", password, 0, name);
+		assertEquals(RegisterStatus.BadEmail, status);
+		
+		status = loginModel.register("test", password, 0, name);
+		assertEquals(RegisterStatus.BadEmail, status);
+	}
+
+	@Test
+	void registerBadName() {
+		RegisterStatus status = loginModel.register(email, password, 0, "");
+		assertEquals(RegisterStatus.BadName, status);
+		
+		status = loginModel.register(email, password, 0, "h");
+		assertEquals(RegisterStatus.BadName, status);
+	}
+
+	@Test
+	void registerBadPassword() {
+		RegisterStatus status = loginModel.register(email, "", 0, name);
+		assertEquals(RegisterStatus.BadPassword, status);
+		
+		status = loginModel.register(email, "123", 0, name);
+		assertEquals(RegisterStatus.BadPassword, status);
+	}
+		
 }
